b2c81a331fbcb93fbaa83f4978691a26e2005688
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-05-21 11:34:52 +0100

    Save Issuer: set enabled.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- yasuke-server/src/controllers/yasuke.controller.ts
+++ yasuke-server/src/controllers/yasuke.controller.ts
@@ -12,88 +12,134 @@ import { Response, ResponseUtils } from 'src/utils';
 @ApiTags('yasuke')
 @Controller('yasuke')
 export class YasukeController {
-    constructor(
-        private configService: ConfigService,
-        private yasukeService: YasukeService,
-        private tokenService: TokenService,
-        private auctionService: AuctionService
-    ) { }
+  constructor(
+    private configService: ConfigService,
+    private yasukeService: YasukeService,
+    private tokenService: TokenService,
+    private auctionService: AuctionService,
+  ) {}
 
-    @Get('/get-token-info/:tokenId')
-    async getTokenInfo(@Param("tokenId") tokenId: number): Promise<Response> {
-        return ResponseUtils.getSuccessResponse(await this.tokenService.getTokenInfo(tokenId));
-    }
+  @Get('/get-token-info/:tokenId')
+  async getTokenInfo(@Param('tokenId') tokenId: number): Promise<Response> {
+    return ResponseUtils.getSuccessResponse(
+      await this.tokenService.getTokenInfo(tokenId),
+    );
+  }
 
-    @Get('/get-auction-info/:tokenId/:auctionId')
-    async getAuctionInfo(@Param("tokenId") tokenId: number, @Param("auctionId") auctionId: number): Promise<Response> {
-        return ResponseUtils.getSuccessResponse(await this.auctionService.getAuctionInfo(tokenId, auctionId));
-    }
+  @Get('/get-auction-info/:tokenId/:auctionId')
+  async getAuctionInfo(
+    @Param('tokenId') tokenId: number,
+    @Param('auctionId') auctionId: number,
+  ): Promise<Response> {
+    return ResponseUtils.getSuccessResponse(
+      await this.auctionService.getAuctionInfo(tokenId, auctionId),
+    );
+  }
 
-    @Get("/get-contract-address")
-    async getContractAddress(): Promise<Response> {
-        return ResponseUtils.getSuccessResponse(await this.configService.get<string>('CONTRACT_ADDRESS'));
-    }
+  @Get('/get-contract-address')
+  async getContractAddress(): Promise<Response> {
+    return ResponseUtils.getSuccessResponse(
+      await this.configService.get<string>('CONTRACT_ADDRESS'),
+    );
+  }
 
-    @Post('/save-issuer')
-    @Roles("api")
-    @ApiSecurity('api-key')
-    async saveIssuer(@Body() issuer: Issuer): Promise<Response> {
-        return ResponseUtils.getSuccessResponse(await this.yasukeService.saveIssuer(issuer));
-    }
+  @Post('/save-issuer')
+  @Roles('api')
+  @ApiSecurity('api-key')
+  async saveIssuer(@Body() issuer: Issuer): Promise<Response> {
+    return ResponseUtils.getSuccessResponse(
+      await this.yasukeService.saveIssuer(issuer),
+    );
+  }
 
-    @Get('is-issuer/:address')
-    async isIssuer(@Param("address") address: string): Promise<Response> {
-        return ResponseUtils.getSuccessResponse(await this.yasukeService.isIssuer(address));
-    }
+  @Get('is-issuer/:address')
+  async isIssuer(@Param('address') address: string): Promise<Response> {
+    return ResponseUtils.getSuccessResponse(
+      await this.yasukeService.isIssuer(address),
+    );
+  }
 
-    @Post('issue-token/')
-    @Roles("api")
-    @ApiSecurity('api-key')
-    async issueToken(@Body() issueToken: IssueToken): Promise<Response> {
-        return ResponseUtils.getSuccessResponse(await this.tokenService.issueToken(issueToken));
-    }
+  @Post('issue-token/')
+  @Roles('api')
+  @ApiSecurity('api-key')
+  async issueToken(@Body() issueToken: IssueToken): Promise<Response> {
+    return ResponseUtils.getSuccessResponse(
+      await this.tokenService.issueToken(issueToken),
+    );
+  }
 
-    @Get('list-tokens')
-    async listTokens(@Query('page') page: number, @Query('limit') limit: number): Promise<Response> {
-        return ResponseUtils.getSuccessResponse(await this.tokenService.listTokens({
-            page,
-            limit,
-            route: '/v3/assets',
-        }));
-    }
+  @Get('list-tokens')
+  async listTokens(
+    @Query('page') page: number,
+    @Query('limit') limit: number,
+  ): Promise<Response> {
+    return ResponseUtils.getSuccessResponse(
+      await this.tokenService.listTokens({
+        page,
+        limit,
+        route: '/v3/assets',
+      }),
+    );
+  }
 
-    @Get('list-tokens/by-owner/:owner')
-    async listTokensByOwner(@Query('page') page: number, @Query('limit') limit: number, @Param("owner") owner: string): Promise<Response> {
-        return ResponseUtils.getSuccessResponse(await this.tokenService.listTokensByOwner({
-            page,
-            limit,
-            route: '/v3/assets',
-        }, owner));
-    }
+  @Get('list-tokens/by-owner/:owner')
+  async listTokensByOwner(
+    @Query('page') page: number,
+    @Query('limit') limit: number,
+    @Param('owner') owner: string,
+  ): Promise<Response> {
+    return ResponseUtils.getSuccessResponse(
+      await this.tokenService.listTokensByOwner(
+        {
+          page,
+          limit,
+          route: '/v3/assets',
+        },
+        owner,
+      ),
+    );
+  }
 
-    @Post('start-auction')
-    @Roles("api")
-    @ApiSecurity('api-key')
-    async startAuction(@Body() sa: StartAuction): Promise<Response> {
-        return ResponseUtils.getSuccessResponse(await this.auctionService.startAuction(sa));
-    }
+  @Post('start-auction')
+  @Roles('api')
+  @ApiSecurity('api-key')
+  async startAuction(@Body() sa: StartAuction): Promise<Response> {
+    return ResponseUtils.getSuccessResponse(
+      await this.auctionService.startAuction(sa),
+    );
+  }
 
-    @Get('list-auctions/by-token-id/:tokenId')
-    async listAuctionsByTokenId(@Query('page') page: number, @Query('limit') limit: number, @Param("tokenId") tokenId: number): Promise<Response> {
-        return ResponseUtils.getSuccessResponse(await this.auctionService.listAuctionsByTokenId({
-            page,
-            limit,
-            route: '/v3/assets',
-        }, tokenId));
-    }
+  @Get('list-auctions/by-token-id/:tokenId')
+  async listAuctionsByTokenId(
+    @Query('page') page: number,
+    @Query('limit') limit: number,
+    @Param('tokenId') tokenId: number,
+  ): Promise<Response> {
+    return ResponseUtils.getSuccessResponse(
+      await this.auctionService.listAuctionsByTokenId(
+        {
+          page,
+          limit,
+          route: '/v3/assets',
+        },
+        tokenId,
+      ),
+    );
+  }
 
-    @Get('get-block')
-    async getBlock(): Promise<Response> {
-        return ResponseUtils.getSuccessResponse(await this.yasukeService.getBlock());
-    }
+  @Get('get-block')
+  async getBlock(): Promise<Response> {
+    return ResponseUtils.getSuccessResponse(
+      await this.yasukeService.getBlock(),
+    );
+  }
 
-    @Get('get-account-balance/:address')
-    async getAccountBalance(@Param("address") address: string): Promise<Response> {
-        return ResponseUtils.getSuccessResponse(await this.yasukeService.getAccountBalance(address));
-    }    
+  @Get('get-account-balance/:address')
+  async getAccountBalance(
+    @Param('address') address: string,
+  ): Promise<Response> {
+    return ResponseUtils.getSuccessResponse(
+      await this.yasukeService.getAccountBalance(address),
+    );
+  }
 }
--- yasuke-server/src/services/yasuke.service.ts
+++ yasuke-server/src/services/yasuke.service.ts
@@ -8,159 +8,170 @@ import { Issuer } from 'src/models/issuer.model';
 import { Utils } from 'src/utils';
 import { ImageService } from './image.service';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path')
+const path = require('path');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const fs = require('fs')
+const fs = require('fs');
 
 @Injectable()
 export class YasukeService {
-    provider: ethers.providers.JsonRpcProvider;
-    yasukeContract: Contract;
-    yasukeAddress: string;
-    yasukeAbi: string;
-    webProvider: string;
+  provider: ethers.providers.JsonRpcProvider;
+  yasukeContract: Contract;
+  yasukeAddress: string;
+  yasukeAbi: string;
+  webProvider: string;
 
-    @InjectRepository(Issuer)
-    private issuerRepository: Repository<Issuer>;
+  @InjectRepository(Issuer)
+  private issuerRepository: Repository<Issuer>;
 
+  private readonly logger = new Logger(YasukeService.name);
 
-    private readonly logger = new Logger(YasukeService.name);
+  constructor(private configService: ConfigService) {
+    this.logger.debug(ethers.utils.formatEther('600000000000000000'));
+    this.webProvider = this.configService.get<string>('WEB3_PROVIDER');
+    this.yasukeAddress = this.configService.get<string>('CONTRACT_ADDRESS');
+    this.provider = new ethers.providers.JsonRpcProvider(this.webProvider);
+    this.yasukeAbi = JSON.parse(
+      fs.readFileSync(path.resolve('src/abis/Yasuke.json'), 'utf8'),
+    ).abi;
+    this.yasukeContract = new ethers.Contract(
+      this.yasukeAddress,
+      this.yasukeAbi,
+      this.provider,
+    );
+  }
 
-    constructor(
-        private configService: ConfigService,
-    ) {
-        this.logger.debug(ethers.utils.formatEther('600000000000000000'));
-        this.webProvider = this.configService.get<string>('WEB3_PROVIDER');
-        this.yasukeAddress = this.configService.get<string>('CONTRACT_ADDRESS');
-        this.provider = new ethers.providers.JsonRpcProvider(this.webProvider);
-        this.yasukeAbi = JSON.parse(fs.readFileSync(path.resolve('src/abis/Yasuke.json'), 'utf8')).abi;
-        this.yasukeContract = new ethers.Contract(this.yasukeAddress, this.yasukeAbi, this.provider);
-    }
+  async getBlock(): Promise<number> {
+    return await this.provider.getBlockNumber();
+  }
 
-    async getBlock(): Promise<number> {
-        return await this.provider.getBlockNumber();
-    }
+  async getAccountBalance(address: string): Promise<string> {
+    return ethers.utils.formatEther(await this.provider.getBalance(address));
+  }
 
-    async getAccountBalance(address: string): Promise<string> {
-        return ethers.utils.formatEther(await this.provider.getBalance(address));
-    }    
+  async isIssuer(address: string): Promise<boolean> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const dbIssuer = await this.issuerRepository
+          .createQueryBuilder('issuer')
+          .where('blockchainAddress = :bad', { bad: address })
+          .getOne();
+        if (dbIssuer === undefined) {
+          reject('Issuer with blockchain address not found');
+        }
 
-    async isIssuer(address: string): Promise<boolean> {
-        return new Promise(async (resolve, reject) => {
-            try {
-                let dbIssuer = await this.issuerRepository.createQueryBuilder("issuer")
-                    .where("blockchainAddress = :bad", { bad: address })
-                    .getOne();
-                if (dbIssuer === undefined) {
-                    reject("Issuer with blockchain address not found");
-                }
+        if (!dbIssuer.enabled) {
+          reject('Issuer with blockchain address has been blocked');
+        }
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 
-                if (!dbIssuer.enabled) {
-                    reject("Issuer with blockchain address has been blocked");
-                }
-            } catch (error) {
-                reject(error);
-            }
-        });
-    }
+  async saveIssuer(issuer: Issuer): Promise<Issuer> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let dbIssuer = await this.issuerRepository
+          .createQueryBuilder('issuer')
+          .where('blockchainAddress = :bad', { bad: issuer.blockchainAddress })
+          .getOne();
+        if (dbIssuer !== undefined) {
+          reject('Issuer with blockchain address already exists');
+        }
 
-    async saveIssuer(issuer: Issuer): Promise<Issuer> {
-        return new Promise(async (resolve, reject) => {
-            try {
-                let dbIssuer = await this.issuerRepository.createQueryBuilder("issuer")
-                    .where("blockchainAddress = :bad", { bad: issuer.blockchainAddress })
-                    .getOne();
-                if (dbIssuer !== undefined) {
-                    reject("Issuer with blockchain address already exists");
-                }
+        dbIssuer = await this.issuerRepository
+          .createQueryBuilder('issuer')
+          .where('phoneNumber = :bad', { bad: issuer.phoneNumber })
+          .getOne();
+        if (dbIssuer !== undefined) {
+          reject('Issuer with phone number already exists');
+        }
 
-                dbIssuer = await this.issuerRepository.createQueryBuilder("issuer")
-                    .where("phoneNumber = :bad", { bad: issuer.phoneNumber })
-                    .getOne();
-                if (dbIssuer !== undefined) {
-                    reject("Issuer with phone number already exists");
-                }
+        dbIssuer = await this.issuerRepository
+          .createQueryBuilder('issuer')
+          .where('email = :bad', { bad: issuer.email })
+          .getOne();
+        if (dbIssuer !== undefined) {
+          reject('Issuer with email already exists');
+        }
 
-                dbIssuer = await this.issuerRepository.createQueryBuilder("issuer")
-                    .where("email = :bad", { bad: issuer.email })
-                    .getOne();
-                if (dbIssuer !== undefined) {
-                    reject("Issuer with email already exists");
-                }
+        issuer.enabled = true;
+        dbIssuer = await this.issuerRepository.save(issuer);
 
-                dbIssuer = await this.issuerRepository.save(issuer);
+        resolve(dbIssuer);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 
-                resolve(dbIssuer);
-            } catch (error) {
-                reject(error);
-            }
-        });
-    }
+  async getTokenInfo(tokenId: number): Promise<TokenInfo> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const ti = await this.yasukeContract.getTokenInfo(tokenId);
+        this.logger.debug('Token from Blockchain - 1');
+        this.logger.debug(ti);
+        const tokenInfo: TokenInfo = {
+          tokenId: ti[0].toNumber(),
+          owner: ti[1],
+          issuer: ti[2],
+          contractAddress: ti[3],
+          symbol: ti[4],
+          name: ti[5],
+          media: [],
+          dateIssued: 0,
+          hasActiveAuction: false,
+          lastAuctionId: 0,
+          category: '',
+        };
 
-    async getTokenInfo(tokenId: number): Promise<TokenInfo> {
-        return new Promise(async (resolve, reject) => {
-            try {
-                const ti = await this.yasukeContract.getTokenInfo(tokenId);
-                this.logger.debug('Token from Blockchain - 1');
-                this.logger.debug(ti);
-                const tokenInfo: TokenInfo = {
-                    tokenId: ti[0].toNumber(),
-                    owner: ti[1],
-                    issuer: ti[2],
-                    contractAddress: ti[3],
-                    symbol: ti[4],
-                    name: ti[5],
-                    media: [],
-                    dateIssued: 0,
-                    hasActiveAuction: false,
-                    lastAuctionId: 0,
-                    category: '',
-                }
+        this.logger.debug(tokenInfo);
+        resolve(tokenInfo);
+      } catch (error) {
+        if (error.reason === 'TINF') {
+          reject(`Token with id ${tokenId} not found`);
+        } else {
+          reject(error);
+        }
+      }
+    });
+  }
 
-                this.logger.debug(tokenInfo);
-                resolve(tokenInfo);
-            } catch (error) {
-                if (error.reason === 'TINF') {
-                    reject(`Token with id ${tokenId} not found`);
-                } else {
-                    reject(error);
-                }
-            }
-        });
-    }
+  async getAuctionInfo(
+    tokenId: number,
+    auctionId: number,
+  ): Promise<AuctionInfo> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const ai = await this.yasukeContract.getAuctionInfo(tokenId, auctionId);
+        this.logger.debug(`Auction Info From Blockchain: ${ai}`);
+        const auctionInfo: AuctionInfo = {
+          tokenId: ai[0].toNumber(),
+          auctionId: ai[1].toNumber(),
+          owner: ai[2],
+          startBlock: ai[3].toNumber(),
+          endBlock: ai[4].toNumber(),
+          currentBlock: ai[5].toNumber(),
+          sellNowPrice: ethers.utils.formatEther(ai[6] + ''),
+          highestBidder: ai[7],
+          highestBid: ethers.utils.formatEther(ai[8] + ''),
+          cancelled: ai[9],
+          minimumBid: ethers.utils.formatEther(ai[10] + ''),
+          bids: [],
+          _bidders: ai[11],
+          _bids: ai[12],
+          isActive: ai[13],
+        };
 
-    async getAuctionInfo(tokenId: number, auctionId: number): Promise<AuctionInfo> {
-        return new Promise(async (resolve, reject) => {
-            try {
-                const ai = await this.yasukeContract.getAuctionInfo(tokenId, auctionId);
-                this.logger.debug(`Auction Info From Blockchain: ${ai}`)
-                const auctionInfo: AuctionInfo = {                    
-                    tokenId: ai[0].toNumber(),
-                    auctionId: ai[1].toNumber(),
-                    owner: ai[2],
-                    startBlock: ai[3].toNumber(),
-                    endBlock: ai[4].toNumber(),
-                    currentBlock: ai[5].toNumber(),
-                    sellNowPrice: ethers.utils.formatEther(ai[6] + ""),
-                    highestBidder: ai[7],
-                    highestBid: ethers.utils.formatEther(ai[8] + ""),
-                    cancelled: ai[9],
-                    minimumBid: ethers.utils.formatEther(ai[10] + ""),
-                    bids: [],                    
-                    _bidders: ai[11],
-                    _bids: ai[12],
-                    isActive: ai[13],                    
-                }
+        this.logger.debug(auctionInfo);
 
-                this.logger.debug(auctionInfo);
-
-                if (auctionInfo.owner === Utils.address0) {
-                    reject("Auction not found on blockchain");
-                }
-                resolve(auctionInfo);
-            } catch (error) {
-                reject(error);
-            }
-        });
-    }
+        if (auctionInfo.owner === Utils.address0) {
+          reject('Auction not found on blockchain');
+        }
+        resolve(auctionInfo);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 }
